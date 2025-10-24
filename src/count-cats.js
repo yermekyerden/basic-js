db74d6c06b6ed8c44c0190c7363bafc4b58c3177
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-10-24 13:19:33 +0500

    feat(count-cats): implement cat-ears counter

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,19 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  if (!Array.isArray(matrix)) return 0;
+
+  let totalCats = 0;
+
+  for (const row of matrix) {
+    if (!Array.isArray(row)) continue;
+    for (const cell of row) {
+      if (cell === '^^') totalCats += 1;
+    }
+  }
+
+  return totalCats;
 }
 
 module.exports = {
