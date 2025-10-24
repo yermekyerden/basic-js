ced053ca12ffb1d250c90aea2e577b57eaed17cd
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-10-24 17:48:53 +0500

    feat(simple-chain): add link operations, removal validation, finishing

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,37 @@ const { NotImplementedError } = require('../lib');
  *
  */
 const chainMaker = {
+  _links: [],
+
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return chainMaker._links.length;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+
+  addLink(value) {
+    const display = (value === undefined) ? '' : String(value);
+    chainMaker._links.push(display);
+    return chainMaker;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+
+  removeLink(position) {
+    const n = chainMaker._links.length;
+    if (!Number.isInteger(position) || position < 1 || position > n) {
+      chainMaker._links = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    chainMaker._links.splice(position - 1, 1);
+    return chainMaker;
   },
+
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    chainMaker._links.reverse();
+    return chainMaker;
   },
+
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const result = chainMaker._links.map(v => `( ${v} )`).join('~~');
+    chainMaker._links = [];
+    return result;
   },
 };
 
