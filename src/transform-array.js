fb14bdd292f39a9890384444dd5b5632e93965a3
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-10-24 17:05:57 +0500

    feat(transform-array): handle control sequences (--discard/--double)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,66 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+
+  const res = [];
+  const isControl = (v) =>
+    v === '--discard-next' ||
+    v === '--discard-prev' ||
+    v === '--double-next' ||
+    v === '--double-prev';
+
+  let skipNext = false;
+  let prevWasDiscarded = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    const cur = arr[i];
+
+    if (skipNext) {
+      skipNext = false;
+      prevWasDiscarded = false;
+      continue;
+    }
+
+    switch (cur) {
+      case '--discard-next':
+        if (i + 1 < arr.length) {
+          skipNext = true;
+        }
+        prevWasDiscarded = false;
+        break;
+
+      case '--discard-prev':
+        if (!prevWasDiscarded && res.length > 0) {
+          res.pop();
+        }
+        prevWasDiscarded = false;
+        break;
+
+      case '--double-next':
+        if (i + 1 < arr.length && !isControl(arr[i + 1])) {
+          res.push(arr[i + 1]);
+        }
+        prevWasDiscarded = false;
+        break;
+
+      case '--double-prev':
+        if (!prevWasDiscarded && res.length > 0) {
+          res.push(res[res.length - 1]);
+        }
+        prevWasDiscarded = false;
+        break;
+
+      default:
+        res.push(cur);
+        prevWasDiscarded = false;
+        break;
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
