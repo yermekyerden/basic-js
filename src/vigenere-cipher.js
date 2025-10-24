cfabddfb95810ebe4af733c0b2f607742478e7dd
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-10-24 18:02:10 +0500

    feat(vigenere-cipher): implement VigenereCipheringMachine (direct/reverse)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,52 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect !== false;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    return this.#process(message, key, true);
+  }
+
+  decrypt(message, key) {
+    return this.#process(message, key, false);
+  }
+
+  #process(message, key, isEncrypt) {
+    if (message === undefined || key === undefined)
+      throw new Error('Incorrect arguments!');
+
+    const m = String(message).toUpperCase();
+    const k = String(key).toUpperCase();
+
+    let res = '';
+    let ki = 0;
+
+    for (let i = 0; i < m.length; i++) {
+      const ch = m[i];
+      const code = ch.charCodeAt(0);
+
+      if (code >= 65 && code <= 90) {
+        const kc = k.charCodeAt(ki % k.length);
+        const shift = kc - 65;
+
+        const base = code - 65;
+        const moved = isEncrypt
+          ? (base + shift) % 26
+          : (base - shift + 26) % 26;
+
+        res += String.fromCharCode(moved + 65);
+        ki++;
+      } else {
+        res += ch;
+      }
+    }
+
+    if (!this.isDirect)
+      res = res.split('').reverse().join('');
+
+    return res;
   }
 }
 
