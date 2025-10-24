d478af7e98e5c700276e014824fbed32ac1f3af8
Yermek Yerden <49679790+yermekyerden@users.noreply.github.com>
2025-10-24 19:30:27 +0500

    fix(what-season): return message on missing argument

--- src/what-season.js
+++ src/what-season.js
@@ -12,7 +12,7 @@ const { NotImplementedError } = require('../lib');
  *
  */
 function getSeason(date) {
-  if (arguments.length === 0) return;
+  if (arguments.length === 0) return 'Unable to determine the time of year!';
 
   try {
     if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0)
@@ -25,7 +25,6 @@ function getSeason(date) {
   }
 
   const m = date.getMonth();
-
   if (m === 11 || m === 0 || m === 1) return 'winter';
   if (m >= 2 && m <= 4) return 'spring';
   if (m >= 5 && m <= 7) return 'summer';
